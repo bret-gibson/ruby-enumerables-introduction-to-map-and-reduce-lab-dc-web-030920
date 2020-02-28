d23e197bd440dbc6e047d56471f0f838c07fcbbe
Bret Gibson <bret.gibson@gmail.com>
2020-02-28 05:25:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,8 +42,7 @@ def map_to_square(source_array)
   newArr
 end
 
-
-def reduce_to_total(source_array, starting_point=0)
+def reduce_to_total(source_array, starting_point = 0)
   count = 0
   newVal = starting_point
   while count < source_array.count do
